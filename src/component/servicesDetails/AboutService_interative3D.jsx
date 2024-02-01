5005634a46f858fe28454602ce9a08325380d04f
unknown <vincentoraclas@gmail.com>
2024-02-02 03:30:36 +0900

    545123465432

--- src/component/servicesDetails/AboutService_interative3D.jsx
+++ src/component/servicesDetails/AboutService_interative3D.jsx
@@ -14,7 +14,7 @@ function AboutService_interative3D() {
                 Dimensions: With a compact size of 166mm x 166mm x 296mm, this unit is designed to fit comfortably in a home setting.
               </p>
               <p className="section-desc mb-4">
-                Features: Despite its smaller size, it doesn&apost skimp on features, providing high-resolution holographic projections and interactive capabilities that allow for a personal, immersive educational experience.
+                Features: Despite its smaller size, it doesn&rsquo;t skimp on features, providing high-resolution holographic projections and interactive capabilities that allow for a personal, immersive educational experience.
               </p>
               <p className="section-desc mb-4">
                 Utility: It is ideal for individual user or small groups who wish to benefit from the holographic teaching experience at home.
@@ -30,7 +30,7 @@ function AboutService_interative3D() {
                 Enhanced Capabilities: The increased size allows for a larger projection and potentially more sophisticated sensors and hardware to handle extensive use in commercial settings like schools, learning centers, or public institutions.
               </p>
               <p className="section-desc mb-4">
-                Customization: The size and features of the commercial model can likely be tailored to the specific needs of the institution, ensuring that the unit&aposs capabilities match the scale and scope of the learning environment it will serve.
+                Customization: The size and features of the commercial model can likely be tailored to the specific needs of the institution, ensuring that the unit&rsquo;s capabilities match the scale and scope of the learning environment it will serve.
               </p>
             </div>
           </div>
